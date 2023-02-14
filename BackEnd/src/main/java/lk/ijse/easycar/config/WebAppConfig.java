251d163555306f356b2966295ef8fa322fd313c3
TharuZika DilakZan <maxtharuz4@gmail.com>
2023-02-14 12:28:18 +0530

    Add Depencies & Configurations

--- BackEnd/src/main/java/lk/ijse/easycar/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/easycar/config/WebAppConfig.java
@@ -1,4 +1,11 @@
 package lk.ijse.easycar.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.easycar")
 public class WebAppConfig {
 }
--- BackEnd/src/main/java/lk/ijse/easycar/config/WebRootConfig.java
+++ BackEnd/src/main/java/lk/ijse/easycar/config/WebRootConfig.java
@@ -1,4 +1,16 @@
 package lk.ijse.easycar.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
+@ComponentScan(basePackages = "lk.ijse.easycar")
 public class WebRootConfig {
+
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
